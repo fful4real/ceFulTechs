0b6d3008b03d518537c3f4591702fac417b947ef
Felix FUL CHI <fful4real@yahoo.com>
2018-09-11 00:36:07 +0400

    addValid

---
+++ src/validator/Constraints/ContainsAlphanumeric.php
@@ -0,0 +1,13 @@
+<?php
+// src/Validator/Constraints/ContainsAlphanumeric.php
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class ContainsAlphanumeric extends Constraint
+{
+    public $message = 'The string "{{ string }}" contains an illegal character: it can only contain letters or numbers.';
+}
